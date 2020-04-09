a1c21fd9707870658d058e7d8bc477167b2b65a7
Toni Naumanen <toni.naumis@gmail.com>
2020-04-09 15:19:29 +0300

    3.11 valmis

---
+++ build/precache-manifest.091070d8106c9337f69908e2f786299f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719ae83a3d340ba649adeacbcb293247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01ef5ea1f98cc932d526",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8868a2df4d8e4a61f736",
+    "url": "/static/js/2.28012aa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28012aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ef5ea1f98cc932d526",
+    "url": "/static/js/main.dc2fbdf6.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('build'))
 
 let persons = [
   {
