1e8d4df94e5507d9618bdb765cf841d6f08e7bd0
Toni Naumanen <toni.naumis@gmail.com>
2020-04-09 14:20:07 +0300

    3.9 valmis

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 
 const app = express()
 
-app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.json())
+app.use(cors())
+
 
 let persons = [
   {
@@ -80,7 +83,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
